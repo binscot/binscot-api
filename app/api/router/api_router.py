38c6273d748cfd9241851c686f583fdddd38f689
binscot <binscot0317@gmail.com>
2023-05-19 20:52:56 +0900

    ...

--- app/api/router/api_router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.endpoints import post, user, auth
-
-api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(user.router, prefix="/users", tags=["users"])
-api_router.include_router(post.router, prefix="/posts", tags=["posts"])
