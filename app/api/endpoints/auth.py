97c5965088f4a79cabe3f9c70167f0a6fe3f1c31
binscot <binscot0317@gmail.com>
2023-08-17 08:42:45 +0900

    auth 엔드포인트, 서비스 정리중

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -4,7 +4,6 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from app.data_type.token_type import Token
 from app.database.database import get_db
 from app.dto import signup_res_dto
 from app.schemas import user_schemas, token_schemas
@@ -18,7 +17,7 @@ def create_user(user: user_schemas.UserCreate, db: Session = Depends(get_db)):
     return auth_service.create_user(db, user)
 
 
-@router.post("/login", response_model=Token)
+@router.post("/login", response_model=token_schemas.Token)
 async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)
 ):
