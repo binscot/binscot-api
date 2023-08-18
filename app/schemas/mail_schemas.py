941fcb5171381c4d518f20ad704c3f5b5bba3e7f
binscot <binscot0317@gmail.com>
2023-08-18 19:37:04 +0900

    메일 스키마 수정

--- app/schemas/mail_schemas.py
+++ app/schemas/mail_schemas.py
@@ -5,8 +5,9 @@ class EmailData(BaseModel):
     to_email: EmailStr
     subject: str
     body: str
+    template: str
 
-    @field_validator('to_email', 'subject', 'body')
+    @field_validator('to_email', 'subject', 'body', 'template')
     def not_empty(cls, v):
         if not v or not v.strip():
             raise ValueError(v, '빈 값은 허용되지 않습니다.')
