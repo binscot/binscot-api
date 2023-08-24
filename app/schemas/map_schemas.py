3a7e9a8ea6477e9950fab03f96f821b9f7eced9c
binscot <binscot0317@gmail.com>
2023-08-24 23:04:04 +0900

    response DTO 정의

--- app/schemas/map_schemas.py
+++ app/schemas/map_schemas.py
@@ -1,14 +1,6 @@
 from pydantic import BaseModel, field_validator
 
 
-class BaseResponseDTO(BaseModel):
-    status_code: int
-    data: dict
-
-    class Config:
-        validate_assignment = True
-
-
 class MapData(BaseModel):
     city: str
     country_code: str
@@ -18,3 +10,4 @@ class MapData(BaseModel):
         if not v or not v.strip():
             raise ValueError(v, '빈 값은 허용되지 않습니다.')
         return v
+
--- app/service/map_service.py
+++ app/service/map_service.py
@@ -1,7 +1,7 @@
 import requests
-from fastapi import HTTPException, status
-from app.schemas import map_schemas
+
 from app.core.config import settings
+from app.dto import base_response_dto, map_response
 
 OPENWEATHERMAP_API_KEY = settings.OPENWEATHERMAP_API_KEY
 OPENWEATHERMAP_LOCATION_URL = settings.OPENWEATHERMAP_LOCATION_URL
@@ -16,36 +16,18 @@ def get_location_by_city(map_data):
     data = requests.get(OPENWEATHERMAP_LOCATION_URL, params=params).json()
 
     if not data:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="나라와 도시를 정확히 입력해주세요"
+        return base_response_dto.BaseResponseDTO(
+            status_code=200,
+            data=None,
+            detail='나라와 도시를 정확히 입력해주세요'
         )
     else:
         data = data[0]
 
-    return map_schemas.BaseResponseDTO(
+    response_data = map_response.MapResponseDTO(city=data['local_names']['ko'], country=data['country'],
+                                                lat=data['lat'], lon=data['lon'])
+    return base_response_dto.BaseResponseDTO(
         status_code=200,
-        data=
-        {
-            "city": data['local_names']['ko'],
-            "country": data['country'],
-            "lat": data['lat'],
-            "lon": data['lon']
-        }
-
+        data=response_data.__dict__,
+        detail='success'
     )
-
-    # return map_schemas.MapDataRes(
-    #     status_code=200,
-    #     city=data['local_names']['ko'],
-    #     country=data['country'],
-    #     lat=data['lat'],
-    #     lon=data['lon']
-    # )
-
-    # return {
-    #     "country": data['country'],
-    #     "city": data['local_names']['ko'],
-    #     "lat": data['lat'],
-    #     "lon": data['lon']
-    # }
