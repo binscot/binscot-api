0e640ced30d9261de4d416cf3d05fb3e4993211a
binscot <binscot0317@gmail.com>
2023-08-30 20:28:55 +0900

    카카오 리스폰스 데이터 정의

--- app/service/kakao_service.py
+++ app/service/kakao_service.py
@@ -1,4 +1,5 @@
 from app.core.config import settings
+from app.dto.response_dto import BaseResponseListDTO
 
 KAKAO_TOKEN_URL = settings.KAKAO_TOKEN_URL
 KAKAO_CLIENT_ID = settings.KAKAO_CLIENT_ID
@@ -6,10 +7,17 @@ KAKAO_SEND_URL = settings.KAKAO_SEND_URL
 
 
 def get_access_token_by_refresh_token():
-    return None
+    return BaseResponseListDTO(
+        status_code=200,
+        data=None,
+        detail="서비스 준비중 입니다."
+    )
 
 
 def send_server_state_kakao_message():
-    return None
-
+    return BaseResponseListDTO(
+        status_code=200,
+        data=None,
+        detail="서비스 준비중 입니다."
+    )
 
