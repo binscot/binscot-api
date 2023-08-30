45ac5bfea2fde5d9645f96198115408854a72dab
binscot <binscot0317@gmail.com>
2023-08-30 20:33:20 +0900

    mail 리스폰스 데이터 정의

--- app/api/endpoints/kakao.py
+++ app/api/endpoints/kakao.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter
 
 from app.service import kakao_service
+from app.dto.response_dto import BaseResponseDTO
 
 router = APIRouter()
 
 
-@router.get("/send_server_state")
+@router.get("/send_server_state", response_model=BaseResponseDTO)
 async def send_server_state_kakao_message():
     return kakao_service.send_server_state_kakao_message()
 
 
-@router.post("/token")
+@router.post("/token", response_model=BaseResponseDTO)
 def get_kakao_token():
     return kakao_service.get_access_token_by_refresh_token()
 
--- app/api/endpoints/mail.py
+++ app/api/endpoints/mail.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
+from app.dto.response_dto import BaseResponseDTO
 from app.schemas import mail_schemas
 from app.service import mail_service
 
 router = APIRouter()
 
 
-@router.post("/send")
+@router.post("/send", response_model=BaseResponseDTO)
 async def send_email(email_data: mail_schemas.EmailData):
     return mail_service.send_email(email_data)
